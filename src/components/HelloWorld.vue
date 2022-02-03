456e42bf4c6948b0eceedfb3092431bd3587f744
davidhickey <davidhickey423@gmail.com>
2022-02-03 13:48:47 -0500

    adds filtering to ui

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello" v-if="parkData">
+    <select v-model="selectedFilter">
+      <option v-for="(header, i) in parkData[0]" :key="i" :value="i">
+       {{ i }}
+      </option>
+      </select>
+    <table>
+       <thead>
+        <tr>
+          <th colspan="2">The table header</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <th v-for="(header, i) in parkData[0]" :key="i" class="col-header"> {{ i }} </th>
+          </tr>
+          <tr v-for="(header, i) in parkDataResults" :key="i">
+             <th v-for="(value, name) in header" :key="name" class="col-header"> {{ value }} </th>
+          </tr>
+      </tbody>
+    </table>
+    
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data(){
+    return{
+      parkData: null,
+      selectedFilter: null,
+    }
+  },
+  computed: {
+    filterResults(){
+      return this.parkData.filter(item => item[`${this.selectedFilter}`] === 'Yes');
+    },
+    parkDataResults(){
+      return this.selectedFilter ? this.filterResults : this.parkData;
+    },
+  },
+  methods: {
+    async getParkData(){
+      console.log('getPArkData');
+      let res = await fetch('https://data.townofcary.org/api/v2/catalog/datasets/parks-and-recreation-feature-map/exports/json');
+      if(!res.ok){
+        throw new Error(`${res.status}`);
+      }
+      let data = await res.json();
+      this.parkData = data;
+      console.log('data ', data);
+    }
+  },
+  mounted(){
+    this.getParkData();
   }
 }
 </script>
@@ -54,5 +78,9 @@ li {
 }
 a {
   color: #42b983;
+}
+
+th{
+  border: 1px solid black;
 }
 </style>
